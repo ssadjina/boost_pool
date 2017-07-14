a147cfa4a80f587e8d5d019c495ec9e241a41853
seatrips <seatrips@shiftnrg.nl>
2017-07-14 03:12:41 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,4 +1,4 @@
 sudo python3 /var/www/delegatepool.seatrips.nl/liskpool.py -y
 sudo bash /var/www/delegatepool.seatrips.nl/payments.sh
 sudo cp /var/www/delegatepool.seatrips.nl/poollogs.json /var/www/html/poollogs.json
-##when using crontab under user##
+##when using crontab under user and using a dedicated server, content of docs in html folder##
