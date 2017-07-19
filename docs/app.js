c8edbedcb70b0707f8716df2faa445ae85e1bc52
seatrips <seatrips@shiftnrg.nl>
2017-07-19 20:49:36 +0200

    delegate ron changes

--- docs/app.js
+++ docs/app.js
@@ -4,10 +4,16 @@ app.controller('indexCtrl', function($scope, $http) {
     $scope.accounts = [];
     $scope.lastpayout = 0;
     $scope.nextpayout = 0;
+    $scope.forged = 0;
+    $scope.topay = 0;
+    $scope.weight = 0;
 
     $http.get ('poollogs.json').then (function (res) {
         $scope.lastpayout = res.data.lastpayout * 1000;
         $scope.nextpayout = moment ($scope.lastpayout).add (1, 'week').valueOf();
+        $scope.forged = res.data.forged;
+        $scope.topay = res.data.topay;
+        $scope.weight = res.data.weight;
         $scope.accounts = [];
 
         for (addr in res.data.accounts) {
